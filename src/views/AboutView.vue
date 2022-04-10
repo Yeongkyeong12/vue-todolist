ad899230f7f50925eaceea17a9ac604875e3506b
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-04-10 11:19:01 +0900

    Feat: add Todo.vue

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, sint,
+      alias doloribus nihil commodi soluta labore fugiat possimus corrupti sunt
+      excepturi esse pariatur, voluptates praesentium repellat asperiores cum.
+      Harum, asperiores!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, sint,
+      alias doloribus nihil commodi soluta labore fugiat possimus corrupti sunt
+      excepturi esse pariatur, voluptates praesentium repellat asperiores cum.
+      Harum, asperiores!
+    </p>
+  </div>
+</template>
